5b0a8e74be8d21ffaa640bf337a78df053518ec2
violetks <48798197+violetks@users.noreply.github.com>
2019-12-27 21:21:00 +0800

    修复babel.config.js中的一点错误

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,12 @@
 // 发布阶段用的 babel 插件
-// const prodPlugins = []
-// if (ProgressEvent.env.NODE_ENV === 'production') {
-//   prodPlugins.push('transform-remove-console')
-// }
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
 
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
-  // plugins: [...prodPlugins]
+  ],
+  plugins: [...prodPlugins]
 }
